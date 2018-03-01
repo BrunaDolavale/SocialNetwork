9f9c29197a7fa11357592486aafcb180f4a4a1e0
Bruna von Seehausen de Rezende Dolavale <brunadolavalerj@gmail.com>
2018-03-01 17:44:50 -0300

    HomeController texto alterado

--- BrunaDolavaleSocialNetwork/BrunaDolavaleSocialNetwork.MVC/Controllers/HomeController.cs
+++ BrunaDolavaleSocialNetwork/BrunaDolavaleSocialNetwork.MVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BrunaDolavaleSocialNetwork.MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Testando";
 
             return View();
         }
